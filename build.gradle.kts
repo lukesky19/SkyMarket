c30b5ae1369d3b949b5b82238b564f9e15805385
lukeskywlker19 <thelukeskywlker19@gmail.com>
2026-07-07 09:44:21 -0400

    Bump paper api to 26.2. Bump SkyLib dependency to 2.0.2.0. Add 26.2 support. Update gradle wrapper to 9.6.1.
    Signed-off-by: lukeskywlker19 <thelukeskywlker19@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "com.github.lukesky19"
-version = "3.1.0.1"
+version = "3.1.1.0"
 
 repositories {
     mavenLocal()
@@ -20,10 +20,10 @@ repositories {
 
 dependencies {
     // Paper
-    compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
+    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
 
     // SkyLib
-    compileOnly("com.github.lukesky19:SkyLib:2.0.0.0")
+    compileOnly("com.github.lukesky19:SkyLib:2.0.2.0")
 
     // Integration
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
